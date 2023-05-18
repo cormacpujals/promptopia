ed945ac2d49271440554433e201dac3db72e3068
Cormac Pujals <110591827+cormacpujals@users.noreply.github.com>
2023-05-17 17:02:27 -1000

    setup and homepage

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+  <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">
+      Discover and Share
+      <br className="max-md:hidden" />
+      <span className="orange_gradient">AI-Powered Prompts</span>
+    </h1>
+    <p>Promptopia is an open source AI prompting tool for the modern world. Discover, create, and share creative AI prompts.</p>
+    {/* Feed */}
+  </section>
+  )
+}
+
+export default Home;
